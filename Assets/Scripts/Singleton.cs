ca000ed985988787abb31d4a553def8f780f493b
Lucio Misael Aquino <luquio@icloud.com>
2021-07-16 15:02:12 -0300

    added singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+		
+	private static T s_Instance;
+
+    public static T Instance
+	{ 
+		get {
+			return s_Instance;
+		}
+	}
+		
+	public virtual void Awake ()
+	{
+		s_Instance = (T)(object)this;
+	}
+}
